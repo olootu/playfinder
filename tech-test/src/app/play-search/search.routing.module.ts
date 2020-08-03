d2c63f853bc34d7543961abf178b1705b8a427b9
Ola Ogunyemi <olootu6831@yahoo.co.uk git config --global user.email olootu6831@yahoo.co.uk>
2020-08-03 12:57:01 +0100

    removed unsed module

--- tech-test/src/app/play-search/search.routing.module.ts
+++ tech-test/src/app/play-search/search.routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ResultComponent } from './results/result.component';
-import { SearchModule } from './search.module';
 
 
 const routes: Routes = [
