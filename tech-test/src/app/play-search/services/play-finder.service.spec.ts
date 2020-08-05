7e915a4a05e8c09b4d0509470fe4ea78821e2931
Ola Ogunyemi <olootu6831@yahoo.co.uk git config --global user.email olootu6831@yahoo.co.uk>
2020-08-05 19:16:18 +0100

    refactored the test to use HttpClientTestingModule

--- tech-test/src/app/play-search/services/play-finder.service.spec.ts
+++ tech-test/src/app/play-search/services/play-finder.service.spec.ts
@@ -1,32 +1,63 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { PlayFinder } from '../playfinder.model'
 
 import { PlayFinderService } from './play-finder.service';
-import { ApiMockService } from './api-mock.service';
 
-describe('PlayFinderService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientModule],
-    providers: [
-      {
-        provide: PlayFinderService,
-        useClass: ApiMockService
+fdescribe('PlayFinderService', () => {
+  let service: PlayFinderService;
+  let httpMock: HttpTestingController
 
-      }
-    ]
-  }));
-
-  it('should be created', () => {
-    const service: PlayFinderService = TestBed.get(PlayFinderService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PlayFinderService]
+    });
+    service = TestBed.get(PlayFinderService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
-  it('should inject', inject([ApiMockService], (service: ApiMockService) => {
-    expect(service).toBeTruthy();
-  }));
+  afterEach(() => {
+    httpMock.verify();
+  })
 
-  it('should return all Pitches', inject([ApiMockService], (service: ApiMockService) => {
-    const getAll = service.getPitches();
-    expect(getAll).not.toBeNull();
-  }));
+  it('should retrieve pitches from API via GET', () => {
+    const dummyPitchesData: PlayFinder[] = [
+      {
+        type: '',
+        id: '32990',
+        attributes: {
+          starts: '2018-01-01',
+          ends: '2018-01-01',
+          price: '15:00',
+          admin_fee: '0:00',
+          currency: '0:00',
+          availabilities: 0
+        }
+      },
+      {
+        type: '',
+        id: '32990',
+        attributes: {
+          starts: '2018-01-01',
+          ends: '2018-01-01',
+          price: '15:00',
+          admin_fee: '0:00',
+          currency: '0:00',
+          availabilities: 0
+        }
+      }
+    ];
+
+    service.getPitches('32990', '2018-01-01', '2018-01-01')
+      .subscribe(res => {
+        expect(res.length).toBe(2);
+      });
+
+    const request = httpMock.expectOne('https://api-v2.pfstaging.xyz/pitches/32990/slots?filter[starts]=2018-01-01&filter[ends]=2018-01-01');
+    expect(request.request.method).toBe('GET');
+
+    request.flush(dummyPitchesData);
+
+  });
 });
